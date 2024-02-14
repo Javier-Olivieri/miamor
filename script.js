91dc75ce063cbf1b31d3e080a4fea4b05f81a4eb
javiermaxi <131915527+javiermaxi@users.noreply.github.com>
2024-02-14 12:58:21 -0300

    mi amor

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('heart').addEventListener('click', function() {
+    document.getElementById('heart-container').style.display = 'none';
+    document.getElementById('card').style.display = 'block';
+});
+
+document.getElementById('btn').addEventListener('click', function() {
+    Swal.fire({
+        title: '¡Te amo demasiado!',el 
+        html: '<span style="font-size: 50px;color: red;">&#10084;</span>', // Corazón
+        confirmButtonText: 'Cerrar'
+    });
+});
+
+
+
+
